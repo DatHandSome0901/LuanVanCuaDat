8fb801887ae4b0b0485355042b9a4e4e4019f7bc
Nguyen Quoc Dat <nguyenquocdat888888@gmail.com>
2026-05-31 19:55:36 +0700

    chore: trigger reload for trimmed SMTP password

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ from time import perf_counter
 # Prefix API theo version
 api_prefix = f"/api/{settings.VERSION_APP}"
 
-# Tạo instance của FastAPI - Reloaded with SMTP config
+# Tạo instance của FastAPI - Reloaded with SMTP password update
 app = FastAPI(
     title=settings.TITLE_APP,
     docs_url=f"{api_prefix}/docs",
